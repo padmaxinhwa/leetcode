8f52b16dcac0b3e417e45d0aa70a775daaa401df
Inhwa <iam.inhwa@gmail.com>
2026-02-23 21:40:34 +0900

    Add balanced parentheses checker in stack_12909.java
    Implement a solution to check for balanced parentheses using a stack.

---
+++ programmers/stack_12909.java
@@ -0,0 +1,21 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java
+
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for (char c : s.toCharArray()) {
+            if(c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                stack.pop();
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
