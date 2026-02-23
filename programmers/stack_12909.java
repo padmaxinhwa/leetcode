7f2addd5e566265c0802886a55c92341d3e85961
Inhwa <iam.inhwa@gmail.com>
2026-02-23 21:50:54 +0900

    Fix comment formatting for URL in stack_12909.java

--- programmers/stack_12909.java
+++ programmers/stack_12909.java
@@ -1,4 +1,4 @@
-//https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java
+// https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java
 
 import java.util.*;
 
